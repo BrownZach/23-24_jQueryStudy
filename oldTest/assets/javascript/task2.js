1b0388165570e901ee261cfbffcf171a321ff7e4
BrownZach <zachary.brown@vtsd.com>
2024-01-18 11:42:46 -0500

    JQuery Continued

--- oldTest/assets/javascript/task2.js
+++ oldTest/assets/javascript/task2.js
@@ -1,1 +1,15 @@
 /* Put code for the task below */
+/*$(document).ready(function(){
+    $("#red").click(function(){
+        $(".table").css("background-color", "red")
+    });
+    $("#green").click(function(){
+        $(".table").css("background-color", "green")
+    });
+    $("#blue").click(function(){
+        $(".table").css("background-color", "blue")
+    });
+});*/
+$(".color").click(function(){
+    $(".table").css("background-color", $(this).val());
+}
